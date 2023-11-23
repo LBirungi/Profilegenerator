9c439f80dcd78fc6927cdeffafbde0ee73d64654
LBirungi <lillian_birungi_lb@yahoo.co.uk>
2023-11-24 01:47:13 +0300

    update profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,27 @@
 import React, { useEffect, useState } from "react";
+import moment from "moment/moment";
 
 function Profile() {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState();
+  const [topRepositories,setToprepositories] =useState();
+  console.log(userData)
+  function getToprepositories() {
+    fetch(userData?.repos_url)
+      .then((response) => {
+        console.log(response)
+        return response.json();
+      }) 
+      .then((data) => {
+        console.log(data);
+        setToprepositories(data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
+
 
   useEffect(() => {
     const user = localStorage.getItem("user");
@@ -10,8 +30,9 @@ function Profile() {
       const parsedUserData = JSON.parse(user);
       setUserData(parsedUserData);
     }
-  }, []);
-
+    getToprepositories()
+  }, [userData]);
+console.log(topRepositories)
   return (
     <div className="min-h-screen bg-gray-100 py-10">
       <div className="w-full bg-white shadow-lg rounded-lg overflow-hidden">
@@ -19,33 +40,38 @@ function Profile() {
 
         <div className="flex flex-col items-center -mt-16 md:-mt-24">
           <img
-            src=""
+            src={userData?.avatar_url
+            }
             alt="ProfileImage"
             className="h-24 w-24 md:h-36 md:w-36 rounded-full border-4 border-white bg-gray-200 object-cover"
           />
           <h1 className="text-xl md:text-2xl font-semibold text-gray-800 mt-2">
-            Lillian Birungi
+            {userData?.name??userData?.login}
+            {console.log('LILLIAN TESTING',userData)}
           </h1>
-          <h2 className="text-sm md:text-md text-gray-500">@LillianBirungi</h2>
+          <h2 className="text-sm md:text-md text-gray-500">{userData?.company}</h2>
         </div>
 
         <div className="px-4 md:px-10 pb-10">
           <div className="flex justify-between text-sm md:text-base text-gray-600 mt-10">
-            <span>Location</span>
-            <span>Date Joined</span>
+          <span><h6>Location</h6>{userData?.location ? userData?.location : "No Location"}</span>
+            <span><h6>Date Joined</h6>{moment(userData?.created_at).format('DD/MM/YYYY')}</span>
           </div>
 
           <div className="flex justify-around mt-4 p-2">
             <div className="text-center">
-              <span className="block text-xl font-semibold">91</span>
+              <span className="block text-xl font-semibold">{userData?.public_repos
+}</span>
               <span>Repositories</span>
             </div>
             <div className="text-center">
-              <span className="block text-xl font-semibold">24</span>
+              <span className="block text-xl font-semibold">{userData?.followers
+}</span>
               <span>Followers</span>
             </div>
             <div className="text-center">
-              <span className="block text-xl font-semibold">47</span>
+              <span className="block text-xl font-semibold">{userData?.following
+}</span>
               <span>Following</span>
             </div>
           </div>
@@ -57,17 +83,17 @@ function Profile() {
             </h3>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
               {/* Repeat this block for each repository card */}
-              {[...Array(6)].map((_, index) => (
+              {topRepositories?.slice(0,10).map((repository, index) => (
                 <div key={index} className="bg-gray-100 p-4 rounded-lg shadow">
                   <h4 className="text-md font-semibold text-gray-700">
-                    Repository Name
+                     {repository.name}
                   </h4>
                   <p className="text-sm text-gray-600 mt-1">
-                    Short description of the repository...
+                    {repository.description}
                   </p>
                   <div className="flex mt-2">
                     <span className="bg-blue-200 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">
-                      Language
+                    {repository?.language ? repository?.language : "No Language"}
                     </span>
                     {/* Additional tags can be added here */}
                   </div>
